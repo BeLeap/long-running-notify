62220677d119ef85dbd80a97281668d32b65c2ea
Changseo <changseo.jang@riiid.co>
2022-09-01 10:07:10 +0900

    get argv

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,30 @@
 const std = @import("std");
 
+const stdout = std.io.getStdOut().writer();
+
 pub fn main() anyerror!void {
+  var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+  const allocator = gpa.allocator();
+  defer {
+    const leaked = gpa.deinit();
+    if (leaked) @panic("LEAK");
+  }
+
+  const process = std.process;
+  var arg_it = process.args();
+  defer arg_it.deinit();
+
+  while (true) {
+    if (arg_it.next(allocator)) |maybe_arg| {
+      var arg = maybe_arg catch |err| { 
+        std.debug.print("unwrap arg failed", .{});
+        return err;
+      };
+
+      defer allocator.free(arg);
+      std.debug.print("{s} ", .{arg});
+    } else {
+      break;
+    }
+  }
 }
