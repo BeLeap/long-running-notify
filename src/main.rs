9257cbe2754fe454d910cf4e3f84d2c0067bdb12
BeLeap <beleap@beleap.codes>
2022-09-08 22:10:43 +0900

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(author, version, about)]
+struct Cli {
+    #[clap(short, long, value_parser)]
+    target_email: String,
+
+    #[clap(value_parser)]
+    command: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    println!("{:?}", cli.command);
 }
