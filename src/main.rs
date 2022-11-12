1171589f01c759e21f3d9abfe08228c4745b686e
BeLeap <beleap@beleap.codes>
2022-11-12 14:18:20 +0900

    feat:run command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::process::Command;
 
 #[derive(Parser)]
 #[clap(author, version, about)]
@@ -12,5 +13,21 @@ struct Cli {
 
 fn main() {
     let cli = Cli::parse();
-    println!("{:?}", cli.command);
+
+    let shell = match std::env::var("SHELL") {
+        Ok(shell) => shell,
+        Err(_) => "sh".to_string(),
+    };
+    let real_command = cli.command.join(" ");
+
+    let mut command = Command::new(shell);
+    command.args(["-c", &real_command]);
+
+    let output = command.output().unwrap();
+    if output.status.success() {
+        println!("{:?}", output);
+    } else {
+        println!("{:?}", output);
+    }
+
 }
