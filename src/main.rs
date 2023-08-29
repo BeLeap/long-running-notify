e2a08467a540b1068bdef03dcb10928eb9f17f65
BeLeap <beleap@beleap.dev>
2023-08-29 18:31:37 +0900

    use subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,71 @@
-use clap::{Parser, ValueEnum};
+use clap::Parser;
+use clap::{Subcommand, ValueHint};
 use notifier::{email::EmailNotifier, freedesktop::FreedesktopNotifier, Notifier};
-use std::process::Command;
+use std::process::{Command, ExitStatus};
 
 mod notifier;
 
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 #[clap(author, version, about)]
 struct Cli {
-    #[clap(value_enum)]
+    #[clap(subcommand)]
     mode: Mode,
+}
 
+#[derive(Subcommand, Debug, PartialEq)]
+enum Mode {
+    Email(EmailArgs),
+    Notification(NotificationArgs),
+}
+
+#[derive(Parser, Debug, PartialEq)]
+struct EmailArgs {
     #[clap(short, long)]
     target_email: Option<String>,
 
+    #[clap(value_hint = ValueHint::CommandWithArguments)]
+    command: Vec<String>,
+}
+
+#[derive(Parser, Debug, PartialEq)]
+struct NotificationArgs {
     #[clap()]
     command: Vec<String>,
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
-enum Mode {
-    Email,
-    Notification,
+fn run_command(cmd: &str) -> ExitStatus {
+    let shell = match std::env::var("SHELL") {
+        Ok(shell) => shell,
+        Err(_) => "sh".to_string(),
+    };
+
+    let mut command = Command::new(shell);
+    command.args(["-c", cmd]);
+
+    command.status().unwrap()
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    let shell = match std::env::var("SHELL") {
-        Ok(shell) => shell,
-        Err(_) => "sh".to_string(),
-    };
-    let real_command = cli.command.join(" ");
-
-    let mut command = Command::new(shell);
-    command.args(["-c", &real_command]);
-
-    let output = command.status().unwrap();
-
     match cli.mode {
-        Mode::Email => {
-            let target_email = cli
+        Mode::Email(ref args) => {
+            let target_email = args
                 .target_email
+                .clone()
                 .expect("target_email is required for email mode");
-            let notifier = EmailNotifier::new(target_email);
-            notifier.send(real_command, output.to_string()).unwrap();
+            let command = args.command.join(" ");
+            let output = run_command(&command);
+
+            let notifier = EmailNotifier::new(target_email.to_string());
+            notifier.send(command, output.to_string()).unwrap();
         }
-        Mode::Notification => {
+        Mode::Notification(ref args) => {
+            let command = args.command.join(" ");
+            let output = run_command(&command);
+
             let notifier = FreedesktopNotifier::new();
-            notifier.send(real_command, output.to_string()).unwrap();
+            notifier.send(command, output.to_string()).unwrap();
         }
     }
 }
